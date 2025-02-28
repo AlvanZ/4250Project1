b187fcf7a1e91a06f694c7b432c7034681b15412
AlvanZ <alvanzhuang625@gmail.com>
2025-02-27 18:56:39 -0800

    merge python files

--- WebSearchInput.py
+++ WebSearchInput.py
@@ -4,7 +4,21 @@ from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 from collections import deque
 import re
+# 1. Make method to get the seed url
+# 2. GO to page, error handle if it doesn't exist. Then you save that page
+# 3. Check if page is in set, if not then add it and then go through all anchor tags that contain the seed url
+# 4. Recurse to 2
+# Needs set to prevent duplicates, and then language detection.
 
+
+
+def count_html_files(directory):
+    return sum(1 for file in os.listdir(directory) if file.endswith(".html"))
+
+
+def run(pages: int, directoryLocation: str):
+    if(count_html_files(directoryLocation)==pages):
+        return "Job Done"
 # Function to normalize and filter URLs
 def normalize_url(base_url, link, allowed_domains, excluded_domains):
     absolute_url = urljoin(base_url, link)
