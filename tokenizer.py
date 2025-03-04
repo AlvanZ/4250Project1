94391ca8ca5c3ec9d21cb2e7bf18bda6706a19bc
Melvin <melvingit@gmail.com>
2025-03-03 21:52:27 -0800

    modify tokenizer for new folder structure. Remove regex filtering

--- tokenizer.py
+++ tokenizer.py
@@ -3,39 +3,48 @@ import re
 from tqdm import tqdm
 from bs4 import BeautifulSoup
 
-os.makedirs("tokenized", exist_ok=True)
+def main():
+    dirs = ["cpp.edu/EN", "taobao.com/JA", "taobao.com/ZH", "yahoo.co.jp/JA"]
 
-# loop through each file within the "repository" folder, using tqdm to display progress
-for file in tqdm(os.listdir("repository")):
-    # open the file and extract the raw html from it
-    opened_file = open(f"repository/{file}", "r", encoding="utf8")
-    raw_html = opened_file.read()
+    for dir in dirs:
+        os.makedirs(f"tokenized/{dir}", exist_ok=True)
+        tokenize(dir)
 
-    # use Beautiful Soup to parse the html
-    soup = BeautifulSoup(raw_html, features="html.parser")
+def tokenize(path):
+    # loop through each file within the specificed folder, using tqdm to display progress
+    for file in tqdm(os.listdir(path)):
+        # open the file and extract the raw html from it
+        opened_file = open(f"{path}/{file}", "r", encoding="utf8")
+        raw_html = opened_file.read()
 
-    # remove all script and style elements from the html
-    for script in soup(["script", "style"]):
-        script.extract()
+        # use Beautiful Soup to parse the html
+        soup = BeautifulSoup(raw_html, features="html.parser")
 
-    # get the text from the html, separating by new lines and removing trailing spaces
-    text = soup.get_text(separator='\n', strip=True)
+        # remove all script and style elements from the html
+        for script in soup(["script", "style"]):
+            script.extract()
 
-    # use regex to remove all non-alphanumeric characters (except for new lines) from the text and replace them with spaces
-    text = re.sub(r'[^\w\n]', ' ', text)
+        # get the text from the html, separating by new lines and removing trailing spaces
+        text = soup.get_text(separator="\n", strip=True)
 
-    # split the text by spaces and new lines into a list of words
-    words = text.split()
+        # use regex to remove all non-alphanumeric characters (except for new lines) from the text and replace them with spaces
+        #text = re.sub(r'[^\w\n]', ' ', text)
 
-    # join all the words together, putting a new line between each word
-    text = "\n".join(words)
+        # split the text by spaces and new lines into a list of words
+        words = text.split()
 
-    # create a new file for the tokenized document
-    tokenized_file = open(f"tokenized/{file}", "w", encoding="utf8")
+        # join all the words together, putting a new line between each word
+        text = "\n".join(words)
 
-    # write the new tokenized document onto the new file
-    tokenized_file.write(text)
+        # create a new file for the tokenized document
+        tokenized_file = open(f"tokenized/{path}/{file}", "w", encoding="utf8")
 
-    # close the files
-    opened_file.close()
-    tokenized_file.close()
+        # write the new tokenized document onto the new file
+        tokenized_file.write(text)
+
+        # close the files
+        opened_file.close()
+        tokenized_file.close()
+
+if __name__ == "__main__":
+    main()
